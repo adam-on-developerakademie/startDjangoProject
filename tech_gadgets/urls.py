490e8c98e93e9d6cbcd41d21c2a6f8585dca9467
adamp <adam.piskorek@gmx.de>
2025-12-18 01:34:37 +0100

    dummy_date schow under tech_gadgets - gadgets

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tech_gadgets.views import start_page_view
+from tech_gadgets.views import start_page_view, single_gadget_view
 
 urlpatterns = [
-    path('', start_page_view, name='start-page'),  # Beispiel für eine Startseiten-URL
-    ]
+    path("", start_page_view, name="start-page"),  # Beispiel für eine Startseiten-URL
+    path("gadgets/", single_gadget_view, name="single-gadget"),
+]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .dummy_date import gadgets, manufacturers
+
 
 # Create your views here.
 def start_page_view(request):
     return HttpResponse("Hej das hat geklappt! Willkommen auf der Startseite.")
+
+
+def single_gadget_view(request, gadget_id=0):
+    return HttpResponse(f"dein Gadget ist: {gadgets[gadget_id]}.")
