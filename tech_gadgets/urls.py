7687418a0a2f3da4d7d91ea6a1a6d885b338a56e
adamp <adam.piskorek@gmx.de>
2025-12-18 04:22:58 +0100

    ridirect from slugify-url to gadget added

--- tech_gadgets/urls.py
+++ tech_gadgets/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from tech_gadgets.views import start_page_view, single_gadget_view_json, single_gadget_view_JsonResponse, single_gadget_your_string, single_gadget_view_slugify
+from tech_gadgets.views import (
+    single_gadget_int,
+    single_gadget_string,
+    start_page_view,
+    single_gadget_view_json,
+    single_gadget_view_JsonResponse,
+    single_gadget_your_string,
+    single_gadget_view_slugify,
+    single_gadget_view_slugify_url,
+)
 
 urlpatterns = [
     path("", start_page_view, name="start-page"),  # Beispiel für eine Startseiten-URL
     path("gadgets-jason/<int:gadget_id>/", single_gadget_view_json),
     path("gadgets-jsonresponse/<int:gadget_id>/", single_gadget_view_JsonResponse),
     path("your-string/<str:yourString>/", single_gadget_your_string),
-    path("find/<str:yourString>/", single_gadget_view_slugify),
+    path("slugify/<str:yourString>/", single_gadget_view_slugify),
+    
+    path("gadget/<int:gadget_id>/", single_gadget_int),
+    path("gadget/<str:gadget_slug>/", single_gadget_string, name="single_gadget_url"),
+    path("slugify-url/<int:gadget_id>/", single_gadget_view_slugify_url),
 ]
--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, JsonResponse
+from django.urls import reverse
 from .dummy_date import gadgets, manufacturers
 import json
 from django.utils.text import slugify
@@ -24,10 +25,27 @@ def single_gadget_your_string(request, yourString=0):
     return HttpResponse(f"Das ist dein String: {yourString}")
 
 def single_gadget_view_slugify(request, yourString=""):
-    gadget_find = {"result": "Not Found"}
+    gadget_slugify = {"result": "Not Found"}
     
     for gadget in gadgets:
         if slugify(gadget["name"]) ==  slugify(yourString):
-            gadget_find = gadget
+            gadget_slugify = gadget
             break
-    return JsonResponse(gadget_find)
+    return JsonResponse(gadget_slugify)
+
+def single_gadget_int(request, gadget_id=0):
+    return JsonResponse(gadgets[gadget_id])
+
+def single_gadget_string(request, gadget_slug=""):
+    gadget_data = {"result": "Not Found"}
+    
+    for gadget in gadgets:
+        if slugify(gadget["name"]) == slugify(gadget_slug):
+            gadget_data = gadget
+            break
+    return JsonResponse(gadget_data)
+
+def single_gadget_view_slugify_url(request, gadget_id=0):
+    new_slug = slugify(gadgets[gadget_id]["name"])
+    new_url = reverse("single_gadget_url", args=[new_slug])
+    return redirect(new_url)    
